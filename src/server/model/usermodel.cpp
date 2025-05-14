996c5759efc734304b16070b34fa009cffa5869f
05allan1213 <2787554820@qq.com>
2025-05-14 15:50:51 +0800

    初步实现数据模块 - User类与UserModel类

---
+++ src/server/model/usermodel.cpp
@@ -0,0 +1,35 @@
+#include "usermodel.hpp"
+
+#include <iostream>
+
+#include "database.hpp"
+
+// User表的增加方法
+bool insert(User& user)
+{
+    // 1.组装sql语句
+    char sql[1024] = {0};
+    sprintf(sql, "insert into user(name, password, state) values('%s', '%s', '%s')",
+            user.getName().c_str(), user.getPwd().c_str(), user.getState().c_str());
+
+    MySQL mysql;
+    if (mysql.connect())
+    {
+        if (mysql.update(sql))
+        {
+            // 获取插入成功的用户数据生成的主键id
+            user.setId(mysql_insert_id(mysql.getConnection()));
+            return true;
+        }
+    }
+    return false;
+}
+
+// 根据用户号码查询用户信息
+User query(int id) {}
+
+// 更新用户的状态信息
+bool updateState(User user) {}
+
+// 重置用户的状态信息
+void resetState() {}
